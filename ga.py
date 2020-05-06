ee0b49911f1a6ff546085e1a3cbeaed286956fa7
Liam Hanigan <lghanigan01@gmail.com>
2020-05-06 05:27:03 -0400

    Fixed GA, implemented basic plotting
    I've fixed the issues with GA generations, resulting in code that can take an initial population and (optionally) an original puzzle to match, and produces solutions in an attempt to create an acceptable sudoku solution, matching the original puzzle if one is given. I have also created the file mpl.py, which contains code for using matplotlib to plot data from running this GA code.
    
    Additionally, both ga.py and mpl.py now have test case functions, which can be used to demonstrate the code and to test its capabilities. Note in particular that test_case_2() in ga.py highlights a major flaw of our current approach to GA: the mutation function does not introduce new cell values that were not present in the initial population. For this reason, I recommend we strongly consider incorporating other mutation functions as well, such as randomly altering a cell (possibly prioritizing empty cells) to a new value.
    
    I also made a few changes to sudoku.py, but none that should affect functionality with previous code. The changes that I made to ga.py DO affect functionality, make sure that if you call the changed functions, that your arguments are declared in a way to work around the newly-added puzzle argument.
    
    The functions I've added to sudoku.py are as follows:
    
    - empty_count(self): returns the total number of empty cells remaining in this sudoku puzzle
    - matches_original(self, original): returns True if self is a valid (possibly partial) solution to the original puzzle, and False otherwise (for example, if it overwrites a cell that was given a starting value in the original puzzle)
    - print_grid(grid, grid_size): prints a grid directly, without needing to take a Sudoku object - convenient for debugging
    
    Changes to ga.py that DO affect functionality:
    
    - default_evaluation now takes a puzzle argument, which defaults to None
    - iterate_one_generation, as well as all variations on run_ga, now take a puzzle argument if they did not already do so; if no such argument is provided, it defaults to None, and evaluation is handled as though there were no original puzzle to solve
    
    The new content in mpl.py:
    
    - run_ga_plot_data: a method that works much like run_ga_get_data, but this one will plot the data with matplotlib (remember that you need to call pyplot.show() to view this plot; multiple plots can be drawn before viewing the result, this way)
    - the above method accepts a mode argument, which defines how data is to be gathered; this can be set to gather the average or best sum of wvalues in a generation, and can display data from every generation, only every generation matching a power of 10, or only the final generation
    
    TODO:
    - implement alternative GA methods
    - ensure algorithm effectiveness working with initial puzzles
    - ensure algorithm can introduce values not in initial population
    -

--- ga.py
+++ ga.py
@@ -5,7 +5,7 @@
 import array, random, sudoku, math, itertools
 from deap import base, creator, tools
 
-DEBUG = False
+DEBUG = True
 
 def random_grid(grid_size = 9):
 
@@ -14,7 +14,7 @@ def random_grid(grid_size = 9):
 def initialize_toolbox(grid_size=9, toolbox = base.Toolbox()):
 
     # Initialize the fitness method to be a negative score equal to # of constraints broken
-    creator.create("MinErrors", base.Fitness, weights=(-1.0,))
+    creator.create("MinErrors", base.Fitness, weights=(-10.0, -1.0, -10.0))
     # Initialize the random_solution() method and put it in Toolbox
     # This method generates a Sudoku grid containing (N**2) cells, where N is the specified grid size;
     # Each cell contains a value between 0 and grid_size, with 0 representing an empty cell
@@ -38,16 +38,20 @@ def swap_cells_once(sudoku): # Takes a sudoku puzzle as a list of values, swaps 
 
     return sudoku
 
-def default_evaluation(sudo):
+def default_evaluation(sudo, puzzle = None):
 
-    return (sudo.constraints_count(),)
+    if sudo.matches_original(puzzle):
+
+        return (sudo.constraints_count(), sudo.empty_count(), 0.0)
+
+    return (sudo.constraints_count(), sudo.empty_count(), 1.0)
 
 def register_evo_functions(
     toolbox,
     evalfunc = default_evaluation, # This method __MUST__ be the evaluation function that takes a guess as a list and returns the number of broken constraints
     matefunc = tools.cxTwoPoint, # This method will be used to get the offspring of two guesses
     mutatefunc = swap_cells_once, # This method will be applied to mutate guesses
-    selectfunc = tools.selRoulette # This method will be used to select the best guesses in a population
+    selectfunc = lambda individuals, k : tools.selTournament(individuals, k, tournsize = 4) # This method will be used to select the best guesses in a population
     ):
 
     toolbox.register("mate", lambda sudo1, sudo2 : [toolbox.sudoku(grid) for grid in matefunc(sudo1.export_grid(), sudo2.export_grid())])
@@ -78,23 +82,26 @@ def default_match_method(parents): # Takes a set of parents, and returns all pos
 
 def default_recombination(population, parents, children): # Takes the original population, the selected parental candidates, and their offspring, and returns a new population
 
-    return parents + children # This default method simply chooses the parents and the children, ignoring any unselected candidates
+    length = len(population) # Take the original length of the population
+    numChildren = len(children) # And the length of the child population
 
-def default_breed_method(parent_set, matefunc): # Takes a set of parents [P1, ..., Pn] and a mate function, and returns the result of replacing P1 and P2 with their child until the only element in the list is a single child
+    if (length > numChildren): # Ensure population stability by selecting individuals from the original population until there are the same number of individuals in the new population
 
-    if len(parent_set) == 1:
+        best = tools.selTournament(population, length - numChildren, tournsize = 4)
 
-        return parent_set[0]
+        return best + children
 
-    children = matefunc(parent_set[0], parent_set[1])
-    new_parent_set = children + parent_set[3:]
+    return children
 
-    return default_breed_method(new_parent_set, matefunc)
+def default_breed_method(parent_set, matefunc): 
+
+    return matefunc(parent_set[0], parent_set[1])
 
 def iterate_one_generation(
     population, # The population of puzzle solutions to be iterated upon
     toolbox, # A toolbox that MUST contain functions "mate", "mutate", "select", and "evaluate"
     k, # The number of parents to be selected for crossover
+    puzzle = None,
     match_method = default_match_method, # The method of selecting which parents breed with which, from a pool of selected candidates
     breed_method = default_breed_method, # The method of combining 2 or more parent solutions to make a child solution
     recombine_method = default_recombination # The method of selecting which individuals will ultimately be recombined to make the new population
@@ -102,7 +109,7 @@ def iterate_one_generation(
 
     for individual in population:
 
-        individual.fitness.values = toolbox.evaluate(individual)
+        individual.fitness.values = toolbox.evaluate(individual, puzzle)
 
     parents = toolbox.select(population, k)
     parent_sets = match_method(parents)
@@ -110,19 +117,25 @@ def iterate_one_generation(
 
     for parent_set in parent_sets:
 
-        child = breed_method(parent_set, toolbox.mate)
-        children.append(toolbox.mutate(child))
+        childs = [toolbox.clone(ind) for ind in parent_set]
+        childs = breed_method(childs, toolbox.mate)
+        children += [toolbox.mutate(child) for child in childs]
 
     new_population = recombine_method(population, parents, children)
+
+    for p in new_population:
+
+        del p.fitness.values
 
     return new_population
 
 def run_ga(
-    puzzle, # A puzzle represented as a tuple (grid, constraints), where grid is a list of N**2 cells, and constraints is the list of all constraints on this puzzle
     toolbox, # A toolbox that MUST contain functions "mate", "mutate", "select", and "evaluate"
     initial_population, # The initial population
+    puzzle = None, # The original puzzle to be solved, if any
     k = 10, # The number of parents to be selected for crossover in each generation
     generations = 1000, # The number of generations to go through before submitting the final population
+    match_method = default_match_method, # The method of selecting which parents breed with which, from a pool of selected candidates
     breed_method = default_breed_method, # The method of selecting which parents breed with which, from a pool of selected candidates
     recombine_method = default_recombination # The method of selecting which individuals will ultimately be recombined to make the new population
     ):
@@ -131,6 +144,187 @@ def run_ga(
 
     for g in range(generations):
 
-        pop = iterate_one_generation(pop, toolbox, k, breed_method, recombine_method)
+        pop = iterate_one_generation(pop, toolbox, k, puzzle, match_method, breed_method, recombine_method)
+
+        for sudo in pop:
+
+            sudo.fitness.values = toolbox.evaluate(sudo)
+
+            if sudo.goal_check():
+
+                if puzzle == None or sudo.matches_original(puzzle):
+
+                    print("SOLUTION FOUND IN GENERATION:", g)
+
+                    return [sudo]
 
     return pop
+
+def get_wvalues_avg(pop): # Get the average sum of the weighted fitness values for individuals in this population
+
+    total = 0.0
+    
+    for individual in pop:
+
+        total += sum(individual.fitness.wvalues)
+
+    return total / len(pop)
+
+def get_wvalues_best(pop): # Get the best sum of the weighted fitness values for individuals in this population
+
+    best = None
+
+    for individual in pop:
+
+        wvalue = sum(individual.fitness.wvalues)
+
+        if best == None:
+
+            best = wvalue
+
+        else:
+
+            best = min(best, wvalue)
+
+    return best
+
+def run_ga_get_data(
+    toolbox,
+    initial_population,
+    puzzle = None,
+    k = 10,
+    generations = 1000,
+    match_method = default_match_method,
+    breed_method = default_breed_method,
+    recombine_method = default_recombination,
+    mode = "WVALUES_AVG"): # this determines the method of data gathering to be performed in this run
+
+    pop = initial_population
+    valid_modes = ["WVALUES_AVG", "WVALUES_BEST", "WVALUES_AVG_FINAL", "WVALUES_AVG_LOG10"] # Acceptable modes for data collection
+
+    if mode not in valid_modes: # If the mode passed is not acceptable, default to WVALUES_AVG
+
+        mode = valid_modes[0]
+
+    x_data = []
+    y_data = []
+    power = 0
+
+    for g in range(generations):
+
+        pop = iterate_one_generation(pop, toolbox, k, puzzle, match_method, breed_method, recombine_method)
+
+        for sudo in pop:
+
+            sudo.fitness.values = toolbox.evaluate(sudo)
+
+            if sudo.goal_check():
+
+                if puzzle == None or sudo.matches_original(puzzle):
+
+                    print("SOLUTION FOUND IN GENERATION:", g)
+
+                    if mode in ["WVALUES_AVG", "WVALUES_AVG_LOG10", "WVALUES_AVG_FINAL"]:
+
+                        x_data.append(g)
+                        y_data.append(get_wvalues_avg(pop))
+
+                    if mode in ["WVALUES_BEST", "WVALUES_BEST_LOG10", "WVALUES_BEST_FINAL"]:
+
+                        x_data.append(g)
+                        y_data.append(get_wvalues_best(pop))
+
+                    return (pop, x_data, y_data)
+
+        if mode == "WVALUES_AVG": # Data will be the averages of the wvalues fields of each population step
+
+            x_data.append(g)
+            y_data.append(get_wvalues_avg(pop))
+
+        if mode == "WVALUES_AVG_LOG10" and (g % 10**power) == 0: # As WVALUES_AVG, but data points are only made for powers of 10
+
+            x_data.append(g)
+            y_data.append(get_wvalues_avg(pop))
+            power += 1
+
+        if mode == "WVALUES_BEST": # Data will be the best wvalues field among the population at each step
+
+            x_data.append(g)
+            y_data.append(get_wvalues_best(pop))
+
+        if mode == "WVALUES_BEST_LOG10" and (g % 10**power) == 0: # As WVALUES_BEST, but data points are only made for powers of 10
+
+            x_data.append(g)
+            y_data.append(get_wvalues_best(pop))
+
+    if mode == "WVALUES_AVG_FINAL": # As WVALUES_AVG, but the only data point generated is the final one
+
+        x_data = generations
+        y_data = get_wvalues_best(pop)
+            
+    return (pop, x_data, y_data)
+
+def test_case_1():
+
+    toolbox = initialize_toolbox(4)
+    toolbox = register_evo_functions(toolbox)
+    grids = [random_grid(4) for foo in range(10)]
+    pop = [toolbox.sudoku(grid) for grid in grids]
+
+    for sudo in pop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+
+    newPop = run_ga(toolbox, pop, k = 2, generations = 1000)
+
+    for sudo in newPop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+        print(sudo.fitness.wvalues)
+
+def test_case_2():
+
+    toolbox = initialize_toolbox(4)
+    toolbox = register_evo_functions(toolbox)
+    grid = [0,3,0,2,0,0,0,0,0,0,0,0,1,0,2,0]
+    grids = [grid for foo in range(10)]
+    pop = [toolbox.sudoku(grid) for grid in grids]
+    original = toolbox.sudoku(grid, grid_size = 4)
+
+    for sudo in pop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+
+    newPop = run_ga(toolbox, pop, puzzle = original, k = 2, generations = 10000)
+
+    for sudo in newPop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+        print(sudo.fitness.wvalues)
+
+def test_case_3():
+
+    toolbox = initialize_toolbox(4)
+    toolbox = register_evo_functions(toolbox)
+    grids = [random_grid(4) for foo in range(10)]
+    pop = [toolbox.sudoku(grid) for grid in grids]
+
+    for sudo in pop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+
+    (newPop, x_data, y_data) = run_ga_get_data(toolbox, pop, k = 2, generations = 1000, mode = "WVALUES_AVG")
+
+    for sudo in newPop:
+
+        sudo.fitness.values = toolbox.evaluate(sudo)
+        print(sudo.export_grid())
+        print(sudo.fitness.wvalues)
+
+    print(x_data)
+    print(y_data)
